2c4059c30fa8adf64e618ad4f9ee4ea0169dc624
Scott Browning <scottybrowning12345@gmail.com>
2025-01-27 13:55:44 -0500

    setter and getter generated. toString also generated. Further testing to the Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 Person sally = new Person("0000001",  "Sally",  "Brown",  1990);
-        System.out.println(sally);
+        System.out.println(sally.getFirstname());
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -11,4 +11,48 @@ public class Person {
         this.YOB = YOB;
 
     }
+//getter and setter methods
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
+
+    //tostring method
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "ID='" + ID + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", YOB=" + YOB +
+                '}';
+    }
 }
