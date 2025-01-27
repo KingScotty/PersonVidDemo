949a1cb17aece8647dc27f0fdb52d39a9b988b53
Scott Browning <scottybrowning12345@gmail.com>
2025-01-27 14:04:10 -0500

    further testing of setLastname and other system.out.println

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 Person sally = new Person("0000001",  "Sally",  "Brown",  1990);
         System.out.println(sally.getFirstname());
+
+        sally.setLastname("Smith");
+        System.out.println(sally.getLastname());
+
     }
 }
